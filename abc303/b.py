065ddcfb259f694700b8d4ce041713cebf239324
Kazuki Ochiai <kazu910739@gmail.com>
2023-06-07 07:49:28 +0900

    commit some practice files

--- abc303/b.py
+++ abc303/b.py
@@ -1,3 +1,17 @@
 N, M = map(int, input().split())
 A = [list(map(int, input().split())) for _ in range(M)]
-print(A)
+ans = 0
+
+for i in range(1, N + 1):
+    for j in range(i + 1, N + 1):
+        flag = False
+        for k in range(M):
+            for l in range(N - 1):
+                if A[k][l] == i and A[k][l + 1] == j:
+                    flag = True
+                if A[k][l] == j and A[k][l + 1] == i:
+                    flag = True
+        if flag == False:
+            ans += 1
+
+print(ans)
