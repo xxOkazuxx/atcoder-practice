fe22e0f08d5f2bee6bb51949c2465445a8da46cf
Kazuki Ochiai <kazu910739@gmail.com>
2023-06-29 23:28:10 +0900

    delete temlate.py

--- Template.py
+++
@@ -1,8 +0,0 @@
-import io, sys
-
-with open("HandInput.txt") as TxtOpen:
-    INPUT = TxtOpen.read() 
-sys.stdin = io.StringIO(INPUT)
-# --------------------------------------------------------
-# AtCoder Beginner Contest
-# --------------------------------------------------------
