73d5d8e6f7938b5684534c180985324221b68011
Kazuki Ochiai <kazu910739@gmail.com>
2023-07-08 10:13:35 +0900

    UnionFindを追加

--- Cheat-sheet.py
+++ Cheat-sheet.py
@@ -11,9 +11,9 @@ direction_2 = [[0, 1], [0, -1], [1, 0], [-1, 0], [1, 1], [1, -1], [-1 ,1], [-1, 
 # 重複ありの組み合わせ：combinations_with_rep(range(始まり,終わり+1),取る個数)
 # 直積：product(range(始まり,終わり+1),range(始まり,終わり+1)):
 import itertools
+
  
 N,K=4,2
- 
 # 順列：permutations(range(N))
 # seq=(0,1,2,3),(0,1,3,2),(0,2,1,3),(0,2,3,1),...,(3,2,1,0)
 print("[順列]")
@@ -79,3 +79,56 @@ def PrimeFactorize(N):
     # 素因数のリストを返す
     return p
 #-------------------------------------------------------------------------------------
+# Union Find
+from collections import defaultdict
+
+
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    def group_count(self):
+        return len(self.roots())
+
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
