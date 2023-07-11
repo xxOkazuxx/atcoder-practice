48cd57d18dcf1207a4ee67ee3a4a9e3392c2b0a6
Kazuki Ochiai <kazu910739@gmail.com>
2023-07-12 07:21:27 +0900

    二分探索とunionfindを少し勉強

--- Practice/union_find/union_find.py
+++ Practice/union_find/union_find.py
@@ -1,13 +1,16 @@
+#-------------------------------------------------------------------------------------
+# Union Find
+#-------------------------------------------------------------------------------------
 from collections import defaultdict
 
 
-class UnionFind():
+class UnionFind(object):
     def __init__(self, n):
         self.n = n
         self.parents = [-1] * n
 
     # 要素xが属するグループの根を返す
-    def find(self, x):
+    def find(self, x: int) -> int:
         if self.parents[x] < 0:
             return x
         else:
@@ -15,7 +18,7 @@ class UnionFind():
             return self.parents[x]
 
     # 要素xが属するグループと要素yが属するグループとを併合する
-    def union(self, x, y):
+    def union(self, x: int, y: int) -> None:
         x = self.find(x)
         y = self.find(y)
 
@@ -29,29 +32,29 @@ class UnionFind():
         self.parents[y] = x
 
     # 要素xが属するグループのサイズ（要素数）を返す
-    def size(self, x):
+    def size(self, x: int) -> int:
         return -self.parents[self.find(x)]
 
     # 要素x, yが同じグループに属するかどうかを返す
-    def same(self, x, y):
+    def same(self, x: int, y: int) -> bool:
         return self.find(x) == self.find(y)
 
     # 要素xが属するグループに属する要素をリストで返す
     # その後に行う処理によっては集合内包表記やジェネレータ式を使う方が効率的となるパターンもある
-    def members(self, x):
+    def members(self, x: int) -> list:
         root = self.find(x)
         return [i for i in range(self.n) if self.find(i) == root]
 
     # 全ての根の要素をリストで返す
-    def roots(self):
+    def roots(self) -> list:
         return [i for i, x in enumerate(self.parents) if x < 0]
 
     # グループの数を返す
-    def group_count(self):
+    def group_count(self) -> int:
         return len(self.roots())
 
     # {ルート要素: [そのグループに含まれる要素のリスト, ...]}のdefaultdictを返す
-    def all_group_members(self):
+    def all_group_members(self) -> dict:
         group_members = defaultdict(list)
         for member in range(self.n):
             group_members[self.find(member)].append(member)
