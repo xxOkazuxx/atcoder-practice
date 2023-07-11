e7218b84c24a75cc433401091266a5143f1ceb76
Kazuki Ochiai <kazu910739@gmail.com>
2023-07-11 23:41:41 +0900

    union-find整理

---
+++ Practice/union_find/union_find.py
@@ -0,0 +1,70 @@
+from collections import defaultdict
+
+
+class UnionFind():
+    def __init__(self, n):
+        self.n = n
+        self.parents = [-1] * n
+
+    # 要素xが属するグループの根を返す
+    def find(self, x):
+        if self.parents[x] < 0:
+            return x
+        else:
+            self.parents[x] = self.find(self.parents[x])
+            return self.parents[x]
+
+    # 要素xが属するグループと要素yが属するグループとを併合する
+    def union(self, x, y):
+        x = self.find(x)
+        y = self.find(y)
+
+        if x == y:
+            return
+
+        if self.parents[x] > self.parents[y]:
+            x, y = y, x
+
+        self.parents[x] += self.parents[y]
+        self.parents[y] = x
+
+    # 要素xが属するグループのサイズ（要素数）を返す
+    def size(self, x):
+        return -self.parents[self.find(x)]
+
+    # 要素x, yが同じグループに属するかどうかを返す
+    def same(self, x, y):
+        return self.find(x) == self.find(y)
+
+    # 要素xが属するグループに属する要素をリストで返す
+    # その後に行う処理によっては集合内包表記やジェネレータ式を使う方が効率的となるパターンもある
+    def members(self, x):
+        root = self.find(x)
+        return [i for i in range(self.n) if self.find(i) == root]
+
+    # 全ての根の要素をリストで返す
+    def roots(self):
+        return [i for i, x in enumerate(self.parents) if x < 0]
+
+    # グループの数を返す
+    def group_count(self):
+        return len(self.roots())
+
+    # {ルート要素: [そのグループに含まれる要素のリスト, ...]}のdefaultdictを返す
+    def all_group_members(self):
+        group_members = defaultdict(list)
+        for member in range(self.n):
+            group_members[self.find(member)].append(member)
+        return group_members
+
+    # printでの表示用
+    # ルート要素: [そのグループに含まれる要素のリスト]を文字列で返す
+    def __str__(self):
+        return '\n'.join(f'{r}: {m}' for r, m in self.all_group_members().items())
+
+
+uf = UnionFind(6)
+uf.union(0, 1)
+uf.union(1, 2)
+uf.union(0, 3)
+print(uf)
