85ffeed806b6c8ce7e96bb4040a25cf42e5b3d17
Kazuki Ochiai <kazu910739@gmail.com>
2023-11-03 22:07:34 +0900

    AtCoder練習用リポジトリをUpdate

--- README.md
+++ README.md
@@ -1,23 +1,14 @@
 # Atcoder 練習保存用リポジトリ
 
----
+## Keyboard Shortcuts
+- Download problems  
+`Ctrl + Shift + 1` 
 
-## プログラム実行方法
+- Test  
+`Ctrl + Shift + 2`
 
-- `Problems/abc`ディレクトリ配下に問題を用意
-- 問題の名前は`https://atcoder.jp/contests/{コンテスト名}`に記載のある問題名とする必要がある
-- `Ctrl + Shift + B`にて、サンプルケースのテストが実行される。
-- `Ctrl + Shift + D`にて、デバッグの実行が可能。サンプルケースは`input.txt`に記載
+- Submit  
+`Ctrl + Shift + 3`
 
-## テストファイル作成
-
-- `sh make_file.sh abc{コンテスト No}`
-- `Problems/abc`に問題が python ファイルとして作成される。
-
-## Git で練習内容を記録
-
-- `sh git.sh "commitメッセージ"`
-
-## サンプルケースと解答を比較するコマンド
-
-- `code -d ./CodeSample/out.txt  ./CodeSample/answer.txt`
+## oj login(Username, Password)
+`oj login https://atcoder.jp/`
--- templates/atcoder.cpp
+++ templates/atcoder.cpp
@@ -1,7 +1,11 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define ll long long
+#define len(x) ll((x).size())
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep(i, a, b) for (int i = (int)(a); i < (int)(b); i++)
+#define print(x) cout << (x) << endl
 
 #define all(v) v.begin(), v.end()
 /*
@@ -11,10 +15,38 @@ using namespace std;
 
 const int dx[4] = {1, 0, -1, 0};
 const int dy[4] = {0, 1, 0, -1};
+const ll INF = 1LL << 60;
+const ll MOD = 1000000007;
+
+int ctoi(char c) { return int(c - '0'); }
+void ssort(string &s) { sort(begin(s), end(s)); }
+long long GCD(long long a, long long b) {
+  if (b == 0)
+    return a;
+  else
+    return GCD(b, a % b);
+}
+long long LCM(long long a, long long b) { return a / GCD(a, b) * b; }
+vector<pair<long long, long long>> prime_factorize(long long N) {
+  vector<pair<long long, long long>> res;
+  for (long long a = 2; a * a <= N; ++a) {
+    if (N % a != 0)
+      continue;
+    long long ex = 0;
+    while (N % a == 0) {
+      ++ex;
+      N /= a;
+    }
+    res.push_back({a, ex});
+  }
+  if (N != 1)
+    res.push_back({N, 1});
+  return res;
+}
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    return 0;
+  return 0;
 }
