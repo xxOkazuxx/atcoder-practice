0569bf24c017aeaa884b7d8b9f1836c98611d422
xxOkazuxx <87918360+xxOkazuxx@users.noreply.github.com>
2023-11-07 21:51:46 +0900

    整数問題の練習 (#5)

--- src/practice/Primes/Primes.cpp
+++ src/practice/Primes/Primes.cpp
@@ -1,10 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+  素数判定
+*/
+bool is_prime(long long n){
+  if (n == 1) return false;
+  for (long long i = 2; i * i <= n; i++){
+    if (n % i == 0){
+      return false;
+    }
+  }
+  return true;
+}
+
+/*
+  約数列挙
+*/
+vector<long long> enum_divisors(long long n){
+  vector<long long> res;
+  for (int i = 1; i * i <= n; i++){
+    if(n % i == 0){
+      res.push_back(i);
+      if(n / i != i){
+        res.push_back(n / i);
+      }
+    }
+  }
+  sort(res.begin(), res.end());
+  return res;
+}
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
+
+    long long n;
+    cin >> n;
 
     return 0;
 }
